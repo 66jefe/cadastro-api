cfeae4640b7806f124014ce22f81a50d56014014
Jeferson Prestes <jeferson_prestes@outlook.com>
2023-03-07 13:17:11 -0300

    adicionado classe model

--- src/main/java/br/com/jeferson/cadastro/model/PessoaModel.java
+++ src/main/java/br/com/jeferson/cadastro/model/PessoaModel.java
@@ -1,5 +1,19 @@
 package br.com.jeferson.cadastro.model;
 
-public class PessoaModel {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PessoaModel {
+	
+	private Integer id;
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String sexo;
+	private String telefone;
+	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO pessoa(nome, idade, cpf, sexo, telefone) VALUES ('Antonio', 24, '98765432120', 'M', '49999999999')
+INSERT INTO pessoa(idpessoa, nome, idade, cpf, sexo, telefone) VALUES (1, 'Antonio', 24, '98765432120', 'M', '49999999999')
