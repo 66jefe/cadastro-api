b2a1d072206f29e79abfd65800d5ae0b30dd7a3f
Jeferson Prestes <jeferson_prestes@outlook.com>
2023-03-09 21:42:27 -0300

    criado classe de configuração

--- src/main/java/br/com/jeferson/cadastro/controller/PessoaController.java
+++ src/main/java/br/com/jeferson/cadastro/controller/PessoaController.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import br.com.jeferson.cadastro.service.PessoaService;
 
 @RestController
 @RequestMapping("/api/pessoas")
-@CrossOrigin(origins = "*")
 public class PessoaController {
 	
 //	Classe onde fica os end-points de microserviços
